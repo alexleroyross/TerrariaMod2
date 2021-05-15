8ca7d97515245f4f00f5ef6de412b4ff2253a195
Frozelar <frostyfish88@gmail.com>
2021-05-15 19:04:57 -0400

    Create Sword and Sparkle

--- Items/GroovySword.cs
+++ Items/GroovySword.cs
@@ -1,3 +1,5 @@
+using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -8,32 +10,53 @@ namespace FrozelarMod2.Items
 		public override void SetStaticDefaults() 
 		{
 			// DisplayName.SetDefault("GroovySword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-			Tooltip.SetDefault("This is a basic modded sword.");
+			Tooltip.SetDefault("Get the grooves.");
 		}
 
 		public override void SetDefaults() 
 		{
-			item.damage = 50;
+			item.damage = 100;
 			item.melee = true;
-			item.width = 40;
-			item.height = 40;
-			item.useTime = 20;
-			item.useAnimation = 20;
+			item.width = 80;
+			item.height = 80;
+			item.useTime = 40;
+			item.useAnimation = 40;
 			item.useStyle = 1;
-			item.knockBack = 6;
-			item.value = 10000;
+			item.knockBack = 8;
+			item.value = 100000;
 			item.rare = 2;
 			item.UseSound = SoundID.Item1;
 			item.autoReuse = true;
 		}
 
-		public override void AddRecipes() 
+		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DirtBlock, 10);
-			recipe.AddTile(TileID.WorkBenches);
+			recipe.AddIngredient(Terraria.ID.ItemID.MeteoriteBar, 24);
+			recipe.AddIngredient(Terraria.ID.ItemID.ShroomiteBar, 18);
+			recipe.AddIngredient(Terraria.ID.ItemID.HellstoneBar, 12);
+			recipe.AddTile(Terraria.ID.TileID.AdamantiteForge);
 			recipe.SetResult(this);
 			recipe.AddRecipe();
+
+			recipe = new ModRecipe(mod);
+			recipe.AddIngredient(Terraria.ID.ItemID.DirtBlock, 5);
+			recipe.AddTile(Terraria.ID.TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		public override void MeleeEffects(Player player, Rectangle hitbox)
+		{
+			if (Main.rand.Next(3) == 0)
+			{
+				int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, mod.DustType("Sparkle"));
+			}
+		}
+
+		public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
+		{
+			target.AddBuff(BuffID.OnFire, 10);
 		}
 	}
 }
